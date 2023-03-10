7e8bfe1a3815aaa61edea1c1bbad0d79ca1eadb8
Victor Sousa <82920344+victorsousalima@users.noreply.github.com>
2023-03-10 20:06:57 -0300

    implementando a classe User

---
+++ src/main/java/br/com/vitinho/ApiUsers/model/User.java
@@ -0,0 +1,47 @@
+package br.com.vitinho.ApiUsers.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    private String email;
+
+    public User() {}
+
+    public User(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
